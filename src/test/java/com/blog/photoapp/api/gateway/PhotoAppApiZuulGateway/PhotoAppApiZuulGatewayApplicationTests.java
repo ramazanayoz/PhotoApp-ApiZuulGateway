ab4d8cdabbd9f524868f645c224d13d8310974c9
150709039 <ramazanayoz2@posta.mu.edu.tr>
2021-07-31 21:51:41 +0300

    first commit

---
+++ src/test/java/com/blog/photoapp/api/gateway/PhotoAppApiZuulGateway/PhotoAppApiZuulGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.blog.photoapp.api.gateway.PhotoAppApiZuulGateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PhotoAppApiZuulGatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
